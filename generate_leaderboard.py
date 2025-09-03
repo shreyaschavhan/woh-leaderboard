b3a99251bd13df39b0c591b53f7c6a7e15640019
Shreyas Chavhan <shreyaschavhan4@gmail.com>
2025-09-03 18:40:18 +0530

    Update generate_leaderboard.py

--- generate_leaderboard.py
+++ generate_leaderboard.py
@@ -89,6 +89,15 @@ def generate_html_leaderboard(data):
         <title>Consistency Leaderboard</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+      <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4NJMY5MRXY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-4NJMY5MRXY');
+    </script>
         <style>
             :root {{
                 --bg-dark-red: #3a0e0e;
