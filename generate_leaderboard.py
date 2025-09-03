5e27d2e9bd1e7e6e8ce2c0ca30c2e1ce801f967d
Shreyas Chavhan <shreyaschavhan4@gmail.com>
2025-09-03 18:46:28 +0530

    `update`

--- generate_leaderboard.py
+++ generate_leaderboard.py
@@ -93,7 +93,7 @@ def generate_html_leaderboard(data):
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-4NJMY5MRXY"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
+        function gtag() {{ dataLayer.push(arguments); }}
         gtag('js', new Date());
 
         gtag('config', 'G-4NJMY5MRXY');
